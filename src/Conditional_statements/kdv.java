9fd1987d48782e2770b2da6c8a29814d65239f40
Esra Akaydın <akaydinesraa@gmail.com>
2024-03-10 13:57:01 +0300

    kdv calculator

--- src/Conditional_statements/kdv.java
+++ src/Conditional_statements/kdv.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 public class kdv {
     public static void main(String[] args) {
-
+        double tutar , Kdvoran=0.18, Kdvtutar, Kdvlitutar;
+        Scanner input= new Scanner(System.in);
+        System.out.print( "Ücreti giriniz : ");
+        tutar = input.nextDouble();
+        Kdvtutar= tutar * Kdvoran;
+        Kdvlitutar = tutar + Kdvtutar;
+        System.out.println("KDV'siz tutar : "+tutar);
+        System.out.println("KDV tutarı : "+Kdvtutar);
+        System.out.println("Kdv'li tutar : "+Kdvlitutar);
     }
 }
